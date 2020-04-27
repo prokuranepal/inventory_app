d321fba7f85a783dabb9f21b8f39b55a6135a098
zorak070 <s.sucil1234@gmail.com>
2020-04-28 04:20:20 +0545

    Corrected a mistake in inventory header

--- src/screens/ItemListScreen.js
+++ src/screens/ItemListScreen.js
@@ -27,10 +27,8 @@ const ItemListScreen = props => {
 };
 
 ItemListScreen.navigationOptions = navigationData => {
-    const catId = navigationData.navigation.getParam('categoryId');
-    const selectedCategory = CATEGORIES.find(cat => cat.id === catId);
     return {
-        headerTitle: selectedCategory.title
+        headerTitle: "Inventory"
     }
 };
 
