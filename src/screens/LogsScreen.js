9cba89233f9e266200dcdc4ba5505dd7dcb747d0
zorak070 <s.sucil1234@gmail.com>
2020-05-12 02:23:26 +0545

    Side bar added to logs screen

--- src/screens/LogsScreen.js
+++ src/screens/LogsScreen.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { View, Text } from 'react-native';
-
+import HeaderButton from '../components/Component/HeaderButton';
+import { HeaderButtons, Item } from 'react-navigation-header-buttons';
+import Colors from '../constants/Colors'
 
 
 const LogsScreen = props => {
@@ -15,6 +17,25 @@ const LogsScreen = props => {
 };
 
 
+
+LogsScreen.navigationOptions = navData => {
+    return {
+        headerTitle: "Logs",
+        headerStyle: {
+            backgroundColor: Platform.OS === 'android' ? Colors.primaryColor : ''
+        },
+        headerTitleStyle: {
+            fontFamily: 'open-sans'
+        },
+        headerLeft: () => (
+            <HeaderButtons HeaderButtonComponent={HeaderButton} >
+                <Item title="Menu" iconName="ios-menu" onPress={() => {
+                    navData.navigation.toggleDrawer()
+                }} />
+            </HeaderButtons>
+        )
+    }
+}
 
 
 export default LogsScreen;
